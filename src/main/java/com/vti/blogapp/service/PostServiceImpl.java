909484b0d0b735948a0c2788ad2855e70958ed1c
nguyxnhung20 <nguyenduyhung.20052002@gmail.com>
2023-12-05 21:32:50 +0700

    [Add] Update post API

--- src/main/java/com/vti/blogapp/service/PostServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/PostServiceImpl.java
@@ -3,6 +3,7 @@ package com.vti.blogapp.service;
 import com.vti.blogapp.dto.PostDto;
 import com.vti.blogapp.entity.Post;
 import com.vti.blogapp.form.PostCreateForm;
+import com.vti.blogapp.form.PostUpdateForm;
 import com.vti.blogapp.mapper.PostMapper;
 import com.vti.blogapp.repository.PostRepository;
 import lombok.AllArgsConstructor;
@@ -29,7 +30,15 @@ public class PostServiceImpl implements PostService{
     @Override
     public PostDto create(PostCreateForm form) {
         var post = PostMapper.map(form); // chuyển từ form sang entity Post
-        var savesPost =  postRepository.save(post); // Lưu vào database
-        return PostMapper.map(savesPost); // chuyển từ entity Post sang form dto
+        var savedPost =  postRepository.save(post); // Lưu vào database
+        return PostMapper.map(savedPost); // chuyển từ entity Post sang form dto
+    }
+
+    @Override
+    public PostDto update(PostUpdateForm form, Long id) {
+        var post = PostMapper.map(form);
+        post.setId(id);
+        var savedPost = postRepository.save(post);
+        return PostMapper.map(savedPost);
     }
 }
